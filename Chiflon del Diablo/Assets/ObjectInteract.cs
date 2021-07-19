f026aa8f8df275170ec86a16cfcda2bdc0e48bf2
markus <mkrisam@alumnos.uai.cl>
2021-07-19 13:36:27 -0400

    Primera escena de testeo Movimiento basico First Person

---
+++ Chiflon del Diablo/Assets/ObjectInteract.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectInteract : MonoBehaviour
+{
+	public Inventory inventory;
+
+	private void Start()
+	{
+		inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Player")
+		{
+			inventory.fichas++;
+			Destroy(gameObject);
+			Debug.Log(inventory.fichas);
+		}
+		
+	}
+}
